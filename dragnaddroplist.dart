7690984333fa6d71d886a542d6517c6acb5ebb59
Captain Awais <114932015+captainawais@users.noreply.github.com>
2022-11-02 00:28:48 +0500

    Create dragnaddroplist.dart
    Awais Farooq
    sp20m2be009

---
+++ dragnaddroplist.dart
@@ -0,0 +1,64 @@
+import 'package:drag_drop_listview_example/model/draggable_list.dart';
+
+List<DraggableList> allLists = [
+  DraggableList(
+    header: 'IUB',
+    items: [
+      DraggableListItem(
+        title: 'Awais',
+        urlImage:
+            'https://images.unsplash.com/photo-1582979512210-99b6a53386f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+      ),
+      DraggableListItem(
+        title: 'Nadeem',
+        urlImage:
+            'https://images.unsplash.com/photo-1560806887-1e4cd0b6cbd6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3367&q=80',
+      ),
+      DraggableListItem(
+        title: 'Ahtesham',
+        urlImage:
+            'https://images.unsplash.com/photo-1595231776515-ddffb1f4eb73?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+      ),
+    ],
+  ),
+  DraggableList(
+    header: 'IUB APP',
+    items: [
+      DraggableListItem(
+        title: 'Amjad',
+        urlImage:
+            'https://images.unsplash.com/photo-1590502593747-42a996133562?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80',
+      ),
+      DraggableListItem(
+        title: 'Umair',
+        urlImage:
+            'https://images.unsplash.com/photo-1571575173700-afb9492e6a50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80',
+      ),
+      DraggableListItem(
+        title: 'Captain',
+        urlImage:
+            'https://images.unsplash.com/photo-1617112848923-cc2234396a8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80',
+      ),
+    ],
+  ),
+  DraggableList(
+    header: 'iub LmS',
+    items: [
+      DraggableListItem(
+        title: 'Awais Farooq',
+        urlImage:
+            'https://images.unsplash.com/photo-1543218024-57a70143c369?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80',
+      ),
+      DraggableListItem(
+        title: 'Captain Awais',
+        urlImage:
+            'https://images.unsplash.com/photo-1464965911861-746a04b4bca6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+      ),
+      DraggableListItem(
+        title: 'Ahtesahm Arshad',
+        urlImage:
+            'https://images.unsplash.com/photo-1577234286642-fc512a5f8f11?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80',
+      ),
+    ],
+  ),
+];
